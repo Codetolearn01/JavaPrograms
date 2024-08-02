7289510a07e2aade52c085987adda779965bdca2
VIGNESH_MURARISHETTI <162784985+Codetolearn01@users.noreply.github.com>
2024-08-02 10:41:03 +0530

    first

---
+++ src/Basic/PrimeOrNot.java
@@ -0,0 +1,29 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean check = isPrime(n);
+        if(check){
+            System.out.println(n+" is prime");
+        }
+        else{
+            System.out.println(n+" is not a prime");
+        }
+    }
+    public static boolean isPrime(int n){
+        int count = 0;
+        for(int i = 1;i <= n;i++){
+            if(n % i == 0){
+                count++;
+            }
+            if(count>2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
