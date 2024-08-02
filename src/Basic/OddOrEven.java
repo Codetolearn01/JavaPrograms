0f57759a963c82510ccf3d5cc7bccc3b6dcbd7e6
VIGNESH_MURARISHETTI <162784985+Codetolearn01@users.noreply.github.com>
2024-08-02 09:53:44 +0530

    first

--- src/Basic/OddOrEven.java
+++ src/Basic/OddOrEven.java
@@ -1,4 +1,19 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        EvenOdd(n);
+    }
+    public static void EvenOdd(int n){
+        if(n % 2 == 0){
+            System.out.println(n+" is even");
+        }
+        else{
+            System.out.println(n+" is odd");
+        }
+    }
 }
